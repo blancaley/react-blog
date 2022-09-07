65c3ab48d99feb001c2d37a68cedcf6370fa8a51
Blanca Ley <blanca.ley@outlook.com>
2022-09-07 16:44:56 +0200

    Add button to toggle visibility for author info

--- src/components/SinglePost.jsx
+++ src/components/SinglePost.jsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
+
 const SinglePost = ( { title, body } ) => {
+  const [ showUserInfo, setShowUserInfo ] = useState(false);
+
+  const toggleUserInfo = () => {
+    setShowUserInfo(!showUserInfo);
+  }
+
   return (
     <div>
       <h2>{title}</h2>
       <p>{body}</p>
+      <button onClick={toggleUserInfo}>Show author</button>
+      {showUserInfo && <p>Test</p>}
     </div>
   );
 }
